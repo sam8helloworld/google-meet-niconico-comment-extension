559558b03b984c978fc20a5f91dfe76858d655c0
takahiro-wakano <takahiro.wakano@excite.jp>
2020-09-30 05:35:06 +0900

    [feat] コンポーネントの追加

---
+++ src/components/CommandBox.vue
@@ -0,0 +1,94 @@
+<template>
+  <v-card>
+    <v-row justify="center">
+      <v-col cols="4" v-for="(size, sizeIndex) in sizes" :key="sizeIndex">
+        <v-btn @click="sizeSelected(size.value)">
+          <span :class="size.classes">A</span>
+        </v-btn>
+      </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-col>
+        <v-btn v-for="(color, colorIndex) in colors" :key="colorIndex" x-small :color="color.label"
+               @click="colorSelected(color.value)"></v-btn>
+      </v-col>
+    </v-row>
+  </v-card>
+</template>
+
+<script>
+
+export default {
+  name: "CommandBox",
+  data() {
+    return {
+      command: {
+        size: "medium",
+        color: "#fff"
+      },
+      sizes: [
+        {
+          value: "big",
+          classes: ["display-1"]
+        },
+        {
+          value: "medium",
+          classes: ["headline"]
+        },
+        {
+          value: "small",
+          classes: ["body-1"]
+        }
+      ],
+      colors: [
+        {
+          label: "white",
+          value: "#fff"
+        },
+        {
+          label: "red",
+          value: "#f44336"
+        },
+        {
+          label: "pink",
+          value: "#e91E63"
+        },
+        {
+          label: "orange",
+          value: "#ff9800"
+        },
+        {
+          label: "yellow",
+          value: "#ffeB3B"
+        },
+        {
+          label: "green",
+          value: "#4caf50"
+        },
+        {
+          label: "blue",
+          value: "#2196f3"
+        },
+        {
+          label: "purple",
+          value: "#9c27b0"
+        },
+        {
+          label: "black",
+          value: "#000000"
+        },
+      ]
+    }
+  },
+  methods: {
+    colorSelected(colorCode) {
+      this.command.color = colorCode;
+      this.$emit("change", this.command);
+    },
+    sizeSelected(size) {
+      this.command.size = size;
+      this.$emit("change", this.command);
+    }
+  }
+}
+</script>
---
+++ src/components/CommentTemplateBtn.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <v-chip small label outlined v-for="(template, index) in templates" @click="selected(template.value)"
+            :key="index">{{
+        template.label
+      }}
+    </v-chip>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "CommentTemplateBtn",
+  data() {
+    return {
+      templates: [
+        {
+          label: "www",
+          value: "www"
+        },
+        {
+          label: "8888888888888",
+          value: "8888888888888"
+        },
+        {
+          label: "草",
+          value: "草"
+        },
+        {
+          label: "生産性",
+          value: "生産性"
+        },
+        {
+          label: "おつ",
+          value: "おつ"
+        },
+        {
+          label: "あっ",
+          value: "あっ"
+        },
+        {
+          label: "そマ？",
+          value: "そマ？"
+        },
+        {
+          label: "ドドドド",
+          value: "┣¨┣¨┣¨┣¨┣¨┣¨┣¨┣¨┣¨┣¨┣¨┣¨┣¨ ┣¨┣¨┣¨┣¨┣¨┣¨┣¨┣¨┣¨┣¨┣¨┣¨┣¨ ┣¨┣¨┣¨"
+        },
+        {
+          label: "キター",
+          value: "ｷﾀ━(ﾟ∀ﾟ)━!!"
+        },
+        {
+          label: "ぴえん",
+          value: "🥺"
+        },
+        {
+          label: "現場ネコ",
+          value: "･･･ヨシ！\n" +
+              "　　　 ∧　　/ヽ\n" +
+              "　　　/／￣￣＼|\n" +
+              "　　 ∠＿╋＿＿〉\n" +
+              "　　/　①八①　ヽ ＿\n" +
+              "　 工ﾆf(_人_)ｴ二|′)ヽ\n" +
+              "　　＼ヽヽノノ ノ ヘ |\n" +
+              "⊂⌒)_＞―――′イ (＿)\n" +
+              "　`ー､_ノ/￣ヽ　｜\n" +
+              "　　 ＿|｜　 |　｜\n" +
+              "　　(　 人＿ノ　Λ\n" +
+              "　　 ＼ス￣￣ﾚ-Λ ＼\n" +
+              "　　(￣　)　/ /　＼ﾉ＼\n" +
+              "　　 ￣￣　(　ヽ　 ＼_)\n" +
+              "　　　　　　＼ノ\n"
+        }
+      ]
+    }
+  },
+  methods: {
+    selected(value) {
+      this.$emit("selected", value);
+    }
+  }
+}
+</script>
